15c1c13735ad99bb4970a0129e39dd9b051ac92a
Jie Chen <jiechenmc@gmail.com>
2024-02-04 13:14:01 -0500

    add template

---
+++ _template/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import logger from 'morgan'
+import indexRouter from './routes/index'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+
+app.use('/api/payment', indexRouter)
+
+const PORT = 3000
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Payment API Engaged')
+})
