d0d549e23c18b0ece60316ff6003ebc73fe5fac1
Jie Chen <jiechenmc@gmail.com>
2024-02-25 19:54:52 -0500

    vault init

---
+++ _scripts/vault.go
@@ -0,0 +1,58 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	vault "github.com/hashicorp/vault/api"
+)
+
+// This is the accompanying code for the Developer Quick Start.
+// WARNING: Using root tokens is insecure and should never be done in production!
+func main() {
+	config := vault.DefaultConfig()
+
+	config.Address = "http://127.0.0.1:8200"
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		log.Fatalf("unable to initialize Vault client: %v", err)
+	}
+
+	// Authenticate
+	client.SetToken("dev-only-token")
+
+	secretData := map[string]interface{}{
+		"status": "success",
+	}
+
+	// Write a secret
+	_, err = client.KVv2("secret").Put(context.Background(), "access_check", secretData)
+	if err != nil {
+		log.Fatalf("unable to write secret: %v", err)
+	}
+
+	fmt.Println("Secret written successfully.")
+
+	// Read a secret from the default mount path for KV v2 in dev mode, "secret"
+	secret, err := client.KVv2("secret").Get(context.Background(), "access_check")
+	if err != nil {
+		log.Fatalf("unable to read secret: %v", err)
+	}
+
+	value, ok := secret.Data["status"].(string)
+
+	if !ok {
+		log.Fatalf("value type assertion failed: %T %#v", secret.Data["status"], secret.Data["status"])
+	}
+
+	if value != "success" {
+		log.Fatalf("unexpected password value %q retrieved from vault", value)
+	}
+
+	fmt.Println("Access granted!")
+}
