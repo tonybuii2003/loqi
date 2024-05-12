123bcc96f2875481dad2b96f0914bc53bd7a1201
Jie Chen <jiechenmc@gmail.com>
2024-05-11 23:11:37 -0400

    multithread bash script

--- sync.sh
+++ sync.sh
@@ -1,9 +1,19 @@
 if git submodule status | grep --quiet '^-'; then
     git submodule update --init
-    git submodule foreach yarn
 fi
 
 git pull
 git submodule foreach git switch main
 git submodule foreach git pull
-# git submodule foreach yarn
+
+proj_home=$PWD
+
+cd $proj_home/activity && yarn &
+cd $proj_home/ai && yarn &
+cd $proj_home/deck && yarn &
+cd $proj_home/neo_client && yarn &
+cd $proj_home/payment && yarn &
+cd $proj_home/repository && yarn &
+cd $proj_home/session && yarn &
+
+wait
