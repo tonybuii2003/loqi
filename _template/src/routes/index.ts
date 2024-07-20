aebc323472b4d572bbe75e80a3ae375075d90e34
Jie Chen <jiechenmc@gmail.com>
2024-07-20 19:25:41 -0400

    remove gcloud API Gateway and cleanup

--- _template/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('Hello from TEMPLATE API')
-})
-
-export default router
