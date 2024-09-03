6a4304e6a163223265cf575afa24e8455cce36c3
Jie Chen <jiechenmc@gmail.com>
2024-09-03 11:12:28 -0400

    up

--- sync.sh
+++ sync.sh
@@ -8,12 +8,7 @@ git submodule foreach git pull
 
 proj_home=$PWD
 
-cd $proj_home/activity && yarn &
-cd $proj_home/ai && yarn &
-cd $proj_home/deck && yarn &
 cd $proj_home/neo_client && yarn &
 cd $proj_home/payment && yarn &
-cd $proj_home/repository && yarn &
-cd $proj_home/session && yarn &
 
 wait
--- sync_server.sh
+++ sync_server.sh
@@ -8,12 +8,7 @@ git submodule foreach git pull
 
 proj_home=$PWD
 
-cd $proj_home/activity &
-cd $proj_home/ai &
-cd $proj_home/deck &
 cd $proj_home/neo_client &
 cd $proj_home/payment &
-cd $proj_home/repository &
-cd $proj_home/session &
 
 wait
